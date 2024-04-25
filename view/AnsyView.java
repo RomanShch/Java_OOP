df002c235e77e5634a334b9c7334d741047b72d7
Roman <roma.schuckin2014@yandex.ru>
2024-04-25 09:54:53 -0300

    added file view to create interface

---
+++ view/AnsyView.java
@@ -0,0 +1,10 @@
+package view;
+
+public class AnsyView {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_CLEAR_SCREEN = "\033[H";
+
+}
